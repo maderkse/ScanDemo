f35503293c521bd1f9a562c4b42041ef0a9a47d4
maderkse <maderkse@gmail.com>
2018-12-29 17:55:00 +0100

    scandemo init

--- ScanDemo/app/src/main/java/com/maderkse/ScanDemo/MainActivity.java
+++ ScanDemo/app/src/main/java/com/maderkse/ScanDemo/MainActivity.java
@@ -1,14 +1,59 @@
 package com.maderkse.ScanDemo;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import android.app.*;
 import android.os.*;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity 
+public class MainActivity extends Activity implements OnClickListener
 {
+	private Button scanBtn;
+	private TextView formatTxt, contentTxt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+		scanBtn = findViewById(R.id.scan_button);
+		formatTxt = findViewById(R.id.scan_format);
+		contentTxt = findViewById(R.id.scan_content);
+
+		scanBtn.setOnClickListener(this);
     }
+
+	public void onClick(View v){
+		//respond to clicks
+		if(v.getId()==R.id.scan_button){
+			//scan
+			IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+			scanIntegrator.initiateScan();
+		}
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		//retrieve scan result
+	    IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+		if (scanningResult != null) {
+			//we have a result
+			String scanContent = scanningResult.getContents();
+			String scanFormat = scanningResult.getFormatName();
+			formatTxt.setText("FORMAT: " + scanFormat);
+			contentTxt.setText("CONTENT: " + scanContent);
+		} else {
+			Toast toast = Toast.makeText(getApplicationContext(), 
+										 "No scan data received!", Toast.LENGTH_SHORT);
+			toast.show();
+		}
+
+	}
+
 }
